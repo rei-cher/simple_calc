f70da4989f368da74aa6f7c568f882b0a1fa3eb1
Pavel <p4shage@gmail.com>
2026-07-29 18:31:45 -0400

    updated user's input validation and integer overflow checks for arithmetic calculations

--- src/calculator.c
+++ src/calculator.c
@@ -15,36 +15,60 @@ typedef union {
 static void usage(const char *name){
 	printf("Incorrect supply of arguments.\n");
 	printf("Usage: %s <number> <operator> <number>\n", name);
-	printf("Operators: + - * / %\n");
+	printf("Arithmetic operators: + - * / %\n");
+	printf("Bitwise operators: & | ^ << >> <<< >>>\n");
 }
 
-static void validate_and_print_signed_result(int32_t *result){
-	if (INT32_MIN > *result || INT32_MAX < *result){
-		printf("Error: result is out of int32_t boundary");
-	}
-	else {
-		printf("%d\n", *result);
-	}
+// ------OPERATORS-------
+// determine either to use signed or unsigned int32_t
+static int is_calc_op(const char *op){
+	return 0 == strcmp(op, "+") || 0 == strcmp(op, "-") || 0 == strcmp(op, "*") || 0 == strcmp(op, "/") || 0 == strcmp(op, "%");
 }
 
-static void validate_and_print_unsigned_result(uint32_t *result){
-	if (0 > *result || UINT32_MAX < *result){
-		printf("Error: result is out of uint32_t boundary");
-	}
-	else {
-		printf("%u\n", *result);
-	}
+static int is_bit_op(const char *op){
+	return 0 == strcmp(op, "&") || 0 == strcmp(op, "|") || 0 == strcmp(op, "^") || 0 == strcmp(op, "<<") || 0 == strcmp(op, ">>") || 0 == strcmp(op, "<<<") || 0 == strcmp(op, ">>>");
 }
 
-// TODO: input validation function
+
+// -------VALIDATIONS---------
+// input validation functions
 // handle bad inputs, integer overflow, etc
 // usigned int should not be negative (it is never negative)
-static int validate_int(const char *argv){
-	int num = atoi(argv);
+
+static int validate_int32(const char *argv, int32_t *converted_argv){
+	long long num;
+	char *end_ptr = NULL;
+
+	num = strtoll(argv, &end_ptr, 10);
+
 	// out of boundary check
 	if (INT32_MIN > num || INT32_MAX < num){
 		return 1;
 	}
+
+	*converted_argv = (int32_t)num;
+
+	return 0;
+}
+
+static int validate_uint32(const char *argv, uint32_t *converted_argv){
+	unsigned long long num;
+	char *end_ptr = NULL;
+
+	// check if user's tryes to provide negative number for bitwise operations
+	if ('-' == argv[0]){
+		printf("Can not accept negative number for bitwise operations\n");
+		return 1;
+	}
+
+	// check if the uint32_t within the range of 0 and UINT32_MAX
+	num = strtoull(argv, &end_ptr, 10); 
+	if (UINT32_MAX < num){
+		printf("Number is out-of-boundary: %s\n", argv);
+		return 1;
+	}
+
+	*converted_argv = (uint32_t)num;
 
 	return 0;
 }
@@ -59,72 +83,94 @@ int start(int argc, char *argv[]){
 		return 1;
 	}
 	
-	// checking if user's values are in boundaries of int32_t size
-	if (0 != validate_int(argv[1]) || 0 != validate_int(argv[3])){
-		printf("Error: one of the user's input number is out of the boundary of int32_t size");
-		return 1;
-	}
-
-	f_num.signed_value = atoi(argv[1]);
-	s_num.signed_value = atoi(argv[3]);
-
 	op = argv[2];
 
-	// check if the operator falls within the limits length
-	if (0 == strlen(op) || strlen(op) > 3) { 
-		printf("Error: incorrect operator.\n");
+	// check if the operator is valid
+	if (!is_calc_op(op) && !is_bit_op(op)) { 
+		printf("Error: incorrect operator '%s'.\n", op);
 		usage(argv[0]);
 		return 1;
 	}
 
+	// check what type to use for int32_t
+	// validate user's input
+	if (is_calc_op(op)){
+		if (0 != validate_int32(argv[1], &f_num.signed_value) || 0 != validate_int32(argv[3], &s_num.signed_value)){
+			printf("Error: must use int32_t in arithmetic operations\n");
+			return 1;
+		}
+	}
+	else {
+		if (0 != validate_uint32(argv[1], &f_num.unsigned_value) || 0 != validate_uint32(argv[3], &s_num.unsigned_value)){
+			printf("Error: must use uint32_t in bitwise operations\n");
+			return 1;
+		}
+	}
+
 	switch (op[0]){
 		case '+':
-			result.signed_value = add(f_num.signed_value, s_num.signed_value);
-			validate_and_print_signed_result(&result.signed_value);
+			if (0 < s_num.signed_value && INT32_MAX - s_num.signed_value < f_num.signed_value){
+				printf("Error: result of addition is exceeded the size of int32_t\n");
+				return 1;
+			}
+			else if (0 > s_num.signed_value && INT32_MAX - s_num.signed_value > f_num.signed_value){
+				printf("Error: result of addition is exceeded the size of int32_t\n");
+				return 1;
+			}
+
+			printf("%d\n", add(f_num.signed_value, s_num.signed_value));
 			break;
 		case '-':
-			result.signed_value = subtract(f_num.signed_value, s_num.signed_value);
-			validate_and_print_signed_result(&result.signed_value);
+			if (0 < s_num.signed_value && INT32_MAX + s_num.signed_value > f_num.signed_value){
+				printf("Error: result of subtraction is exceeded the size of int32_t\n");
+				return 1;
+			}
+			else if (0 > s_num.signed_value && INT32_MAX + s_num.signed_value < f_num.signed_value){
+				printf("Error: result of subtraction is exceeded the size of int32_t\n");
+				return 1;
+			}
+
+			printf("%d\n", subtract(f_num.signed_value, s_num.signed_value));
 			break;
 		case '*':
-			result.signed_value = multiply(f_num.signed_value, s_num.signed_value);
-			validate_and_print_signed_result(&result.signed_value);
+			if (INT32_MAX / s_num.signed_value > f_num.signed_value){
+				printf("Error: result of mulriplication is exceeded the size of int32_t\n");
+				return 1;
+			}
+
+			printf("%d\n", multiply(f_num.signed_value, s_num.signed_value));
 			break;
 		case '/':
 			if (0 != divide(f_num.signed_value, s_num.signed_value, &result.signed_value)) {
 				printf("Error: can not divide by 0.\n");
 				return 1;
 			}
-			validate_and_print_signed_result(&result.signed_value);
+
+			printf("%d\n", result.signed_value);
 			break;
 		case '%':
 			if (0 != modulo(f_num.signed_value, s_num.signed_value, &result.signed_value)) {
 				printf("Error: can not perform modulo by 0.\n");
 				return 1;
 			}
-			validate_and_print_signed_result(&result.signed_value);
+
+			printf("%d\n", result.signed_value);
 			break;
-		// TODO: should not operate with negative numbers
 		case '&':
-			result.unsigned_value = op_and(f_num.unsigned_value, s_num.unsigned_value);
-			validate_and_print_unsigned_result(&result.unsigned_value);
+			printf("%u\n", op_and(f_num.unsigned_value, s_num.unsigned_value));
 			break;
 		case '|':
-			result.unsigned_value = op_or(f_num.unsigned_value, s_num.unsigned_value);
-			validate_and_print_unsigned_result(&result.unsigned_value);
+			printf("%u\n", op_or(f_num.unsigned_value, s_num.unsigned_value));
 			break;
 		case '^':
-			result.unsigned_value = op_xor(f_num.unsigned_value, s_num.unsigned_value);
-			validate_and_print_unsigned_result(&result.unsigned_value);
+			printf("%u\n", op_xor(f_num.unsigned_value, s_num.unsigned_value));
 			break;
 		case '<':
 			if (0 == strcmp(op, "<<")){
-				result.unsigned_value = l_shift(f_num.unsigned_value, s_num.unsigned_value);
-				validate_and_print_unsigned_result(&result.unsigned_value);
+				printf("%u\n", l_shift(f_num.unsigned_value, s_num.unsigned_value));
 			}
 			else if (0 == strcmp(op, "<<<")){
-				result.unsigned_value = l_rotate(f_num.unsigned_value, s_num.unsigned_value);
-				validate_and_print_unsigned_result(&result.unsigned_value);
+				printf("%u\n", l_rotate(f_num.unsigned_value, s_num.unsigned_value));
 			}
 			else {
 				printf("Error: incorrect operator.\n");
@@ -134,12 +180,10 @@ int start(int argc, char *argv[]){
 			break;
 		case '>':
 			if (0 == strcmp(op, ">>")){
-				result.unsigned_value = r_shift(f_num.unsigned_value, s_num.unsigned_value);
-				validate_and_print_unsigned_result(&result.unsigned_value);
+				printf("%u\n", r_shift(f_num.unsigned_value, s_num.unsigned_value));
 			}
 			else if (0 == strcmp(op, ">>>")){
-				result.unsigned_value = r_rotate(f_num.unsigned_value, s_num.unsigned_value);
-				validate_and_print_unsigned_result(&result.unsigned_value);
+				printf("%u\n", r_shift(f_num.unsigned_value, s_num.unsigned_value));
 			}
 			else {
 				printf("Error: incorrect operator.\n");
