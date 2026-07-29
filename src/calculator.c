3125bb0f97910fbddc348f372d9ba6c9c1f5a478
Pavel <p4shage@gmail.com>
2026-07-28 21:27:15 -0400

    union statement to handle signed and unsigned type
    TODO: implement input_handle function to validate user's input

--- src/calculator.c
+++ src/calculator.c
@@ -6,14 +6,26 @@
 #include "calc_operations.h"
 #include "bit_operations.h"
 
+// union statement to manage both signed and unsigned types
+typedef union {
+	int32_t signed_value;
+	uint32_t unsigned_value;
+} int32_value;
+
 static void usage(const char *name){
 	printf("Incorrect supply of arguments.\n");
 	printf("Usage: %s <number> <operator> <number>\n", name);
 	printf("Operators: + - * / %\n");
 }
 
+// TODO: input handle function
+// handle bad inputs, integer overflow, etc
+static int handle_input(const char *argv){
+
+}
+
 int start(int argc, char *argv[]){
-	int32_t f_num, s_num, result;
+	int32_value f_num, s_num, result;
 	char *op;
 
 	// check if all 3 arguments were supplied
@@ -21,9 +33,9 @@ int start(int argc, char *argv[]){
 		usage(argv[0]);
 		return 1;
 	}
-
-	f_num = atof(argv[1]);
-	s_num = atof(argv[3]);
+	
+	f_num.signed_value = atoi(argv[1]);
+	s_num.signed_value = atoi(argv[3]);
 
 	op = argv[2];
 
@@ -36,45 +48,53 @@ int start(int argc, char *argv[]){
 
 	switch (op[0]){
 		case '+':
-			printf("%d\n", add(f_num, s_num));
+			result.signed_value = add(f_num.signed_value, s_num.signed_value);
+			printf("%d\n", result.signed_value);
 			break;
 		case '-':
-			printf("%d\n", subtract(f_num, s_num));
+			result.signed_value = subtract(f_num.signed_value, s_num.signed_value);
+			printf("%d\n", result.signed_value);
 			break;
 		case '*':
-			printf("%d\n", multiply(f_num, s_num));
+			result.signed_value = multiply(f_num.signed_value, s_num.signed_value);
+			printf("%d\n", result.signed_value);
 			break;
 		case '/':
-			if (0 != divide(f_num, s_num, &result)) {
+			if (0 != divide(f_num.signed_value, s_num.signed_value, &result.signed_value)) {
 				printf("Error: can not divide by 0.\n");
 				return 1;
 			}
 
-			printf("%d\n", result);
+			printf("%d\n", result.signed_value);
 			break;
 		case '%':
-			if (0 != modulo(f_num, s_num, &result)) {
+			if (0 != modulo(f_num.signed_value, s_num.signed_value, &result.signed_value)) {
 				printf("Error: can not perform modulo by 0.\n");
 				return 1;
 			}
 			
-			printf("%d\n", result);
+			printf("%d\n", result.signed_value);
 			break;
 		case '&':
-			printf("%u\n", op_and(f_num, s_num));
+			result.unsigned_value = op_and(f_num.unsigned_value, s_num.unsigned_value);
+			printf("%u\n", result.unsigned_value);
 			break;
 		case '|':
-			printf("%u\n", op_or(f_num, s_num));
+			result.unsigned_value = op_or(f_num.unsigned_value, s_num.unsigned_value);
+			printf("%u\n", result.unsigned_value);
 			break;
 		case '^':
-			printf("%u\n", op_xor(f_num, s_num));
+			result.unsigned_value = op_xor(f_num.unsigned_value, s_num.unsigned_value);
+			printf("%u\n", result.unsigned_value);
 			break;
 		case '<':
 			if (0 == strcmp(op, "<<")){
-				printf("%u\n", l_shift(f_num, s_num));
+				result.unsigned_value = l_shift(f_num.unsigned_value, s_num.unsigned_value);
+				printf("%u\n", result.unsigned_value);
 			}
 			else if (0 == strcmp(op, "<<<")){
-				printf("%u\n", l_rotate(f_num, s_num));
+				result.unsigned_value = l_rotate(f_num.unsigned_value, s_num.unsigned_value);
+				printf("%u\n", result.unsigned_value);
 			}
 			else {
 				printf("Error: incorrect operator.\n");
@@ -84,10 +104,12 @@ int start(int argc, char *argv[]){
 			break;
 		case '>':
 			if (0 == strcmp(op, ">>")){
-				printf("%u\n", r_shift(f_num, s_num));
+				result.unsigned_value = r_shift(f_num.unsigned_value, s_num.unsigned_value);
+				printf("%u\n", result.unsigned_value);
 			}
 			else if (0 == strcmp(op, ">>>")){
-				printf("%u\n", r_rotate(f_num, s_num));
+				result.unsigned_value = r_rotate(f_num.unsigned_value, s_num.unsigned_value);
+				printf("%u\n", result.unsigned_value);
 			}
 			else {
 				printf("Error: incorrect operator.\n");
